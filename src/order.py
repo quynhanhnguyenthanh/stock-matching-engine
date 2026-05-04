114629da4952933d528439900817009de4060ce0
quynhanhnguyenthanh <quynhanh383368@gmail.com>
2026-05-04 19:14:58 +0700

    Update order.py

--- src/order.py
+++ src/order.py
@@ -1,1 +1,10 @@
-
+class Order:
+    __slots__ = ['order_id', 'is_buy', 'price', 'quantity', 'timestamp', 'is_canceled']
+    
+    def __init__(self, order_id: str, is_buy: bool, price: float, quantity: int, timestamp: float):
+        self.order_id = order_id
+        self.is_buy = is_buy 
+        self.price = price
+        self.quantity = quantity
+        self.timestamp = timestamp
+        self.is_canceled = False 
